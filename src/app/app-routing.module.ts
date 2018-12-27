84d5104e5671b4a2a1f3554f5f905e378ecbc8f9
susanwands <Susan.Wands@IssuerDirect.com>
2018-12-27 15:18:24 -0500

    routing and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CampaignsComponent} from 'src/app/campaigns/campaigns.component';
+import { HomeComponent} from 'src/app/home/home.component';
+import { ImpactComponent} from 'src/app/impact/impact.component';
+import { ProfileComponent} from 'src/app/profile/profile.component';
+import { StoriesComponent} from 'src/app/stories/stories.component';
+import { StrategyComponent} from 'src/app/strategy/strategy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'campaigns', component: CampaignsComponent},
+  { path: 'impact', component: ImpactComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'stories', component: StoriesComponent},
+  { path: 'strategy', component: StrategyComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { StrategyComponent } from './strategy/strategy.component';
+import { StoriesComponent } from './stories/stories.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
+import { ImpactComponent } from './impact/impact.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    StrategyComponent,
+    StoriesComponent,
+    CampaignsComponent,
+    ImpactComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,

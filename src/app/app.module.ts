005822566aafacfa061e35301efcbb5072017908
susanwands <Susan.Wands@IssuerDirect.com>
2019-01-10 08:11:36 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,7 +10,7 @@ import { StoriesComponent } from './stories/stories.component';
 import { CampaignsComponent } from './campaigns/campaigns.component';
 import { ImpactComponent } from './impact/impact.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import {MatButtonModule, MatCheckboxModule, MatSlideToggleModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +24,16 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
